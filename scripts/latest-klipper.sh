fcab796486bdd311a6150f83f5b548787e6530a1
Aaron Haun <aaron@haun.guru>
2023-09-02 12:13:43 -0400

    Add missing configs

--- scripts/latest-klipper.sh
+++ scripts/latest-klipper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-repo_url="${1:-https://github.com/Klipper3d/Klipper.git}"
+repo_url="${2:-https://github.com/Klipper3d/Klipper.git}"
 ref="${1-master}"
-git ls-remote -q "${repo_url}" "${ref}" | cut -f 1 -d "	" | cut -c 1-7 
+git ls-remote -q "${repo_url}" "${ref}" | cut -f 1 -d "	"
