8abc8edef3b46cfe99ed843ffb475aedb89978e6
Aaron Haun <aaron@haun.guru>
2024-01-09 19:38:42 +0000

    fix: don't try to get a TTY for building

--- .github/factory-build-all.sh
+++ .github/factory-build-all.sh
@@ -11,7 +11,7 @@ for conf in "${conflist[@]}"; do
 	tmp_out="${tmp_dir}/out"
 	mkdir "${tmp_conf}" "${tmp_out}"
 	cp "$conf" "${tmp_conf}/${conf_name}"
-	docker run --rm -it -v "${tmp_conf}:/mnt/conf:z" -v "${tmp_out}:/mnt/out:z" "ghcr.io/laikulo/klipper-firmware/factory:${1:-laikulo}" -c "/mnt/conf/${conf_name}" -o "/mnt/out"
+	docker run --rm -v "${tmp_conf}:/mnt/conf:z" -v "${tmp_out}:/mnt/out:z" "ghcr.io/laikulo/klipper-firmware/factory:${1:-laikulo}" -c "/mnt/conf/${conf_name}" -o "/mnt/out"
 	cp -r "${tmp_out}/"*"."* "${out_dir}"
 	rm -rf "${tmp_dir}"
 done
