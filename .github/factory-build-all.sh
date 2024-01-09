82e94e197a894512bdc27ff71a9573d961b0acd2
Aaron Haun <aaron@haun.guru>
2024-01-09 19:36:19 +0000

    feat: run all action

---
+++ .github/factory-build-all.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e -x
+typeset -a conflist
+conflist=("factory-configs/"*".config")
+for conf in "${conflist[@]}"; do
+	conf_dir="$(dirname "${conf}")"
+	conf_name="$(basename "${conf}")"
+	out_dir="$PWD/dist"
+	tmp_dir="$(mktemp -d -t klipperfactory.XXXXXXXXXX)"
+	tmp_conf="${tmp_dir}/conf"
+	tmp_out="${tmp_dir}/out"
+	mkdir "${tmp_conf}" "${tmp_out}"
+	cp "$conf" "${tmp_conf}/${conf_name}"
+	docker run --rm -it -v "${tmp_conf}:/mnt/conf:z" -v "${tmp_out}:/mnt/out:z" "ghcr.io/laikulo/klipper-firmware/factory:${1:-laikulo}" -c "/mnt/conf/${conf_name}" -o "/mnt/out"
+	cp -r "${tmp_out}/"*"."* "${out_dir}"
+	rm -rf "${tmp_dir}"
+done
